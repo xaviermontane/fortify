a679b612e0e46cd5ad1119cbcd6be9191b699d96
xaviermontane <xaviermontane3@gmail.com>
2024-12-12 02:57:07 -0500

    feat: Add password validation for length, number, and symbol
    - Implement regex to check for at least one number and one symbol in the password
    - Add if/else statements to prompt user to correct password if requirements are not met
    - Ensure password meets minimum length of 12 characters
    - Display password before and after being fortified

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-import string
-import secrets
+import re
 
 user_pwd = input("\nEnter your password: ")
-
 no_format = True
-while no_format: #Formatting checks [Add more...]
+
+while no_format:
     format_pwd = user_pwd.replace(" ", "")
-    print() if format_pwd == user_pwd else print(f"Your password has been formatted to: {format_pwd}") #Omit formatting if unnecessary
+    print() if format_pwd == user_pwd else print(f"Password formatted to: {format_pwd}") #Omit formatting if unnecessary
     no_format = False
 
-def secure_pass(pwd):
+def fortify_pass(pwd):
     pwd = format_pwd
-    minimum_req = [len(pwd) < 12]
-    #Security checks begin
-    for x in minimum_req:
-        if x is True:
-            print("Your password doesn't meet the minimum requirements")
-        elif x is False:
-            print("Your password meets the minimum requirements")
+    pattern = re.compile(r'^(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).+$')
+    if pattern.match(pwd):
+        print("[√] - Your password meets the minimum requirements\n")
+        minimum_req = True
     else:
-        pass
+        print("[X] - Your password doesn't meet the minimum requirements\n")
+        minimum_req = False
+
+    #Security checks begin
+    while not minimum_req:
+        if len(pwd) < 12:
+            pwd = input("Include at least 12 characters: ")
+        if not re.search(r'[0-9]', pwd):
+            pwd = input("Include at least one number: ")
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', pwd):
+            pwd = input("Include at least one symbol: ")
+        
+        # Re-check if the password now meets the requirements
+        if len(pwd) >= 12 and re.search(r'[0-9]', pwd) and re.search(r'[!@#$%^&*(),.?":{}|<>]', pwd):
+            minimum_req = True
+
     global secure_pwd
     secure_pwd = pwd
-secure_pass(user_pwd)
 
-print(f"\nYour old password was: {user_pwd}")
-print(f"Your secure password is: {secure_pwd}")
+fortify_pass(format_pwd)
+
+print(f"\nYour old password was: {user_pwd}") 
+print(f"Your secure password is: {secure_pwd}\n")
