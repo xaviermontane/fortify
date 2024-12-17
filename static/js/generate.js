7b17737144265ce15a4cb26fbe0dfc7613eece49
F0RTY <xaviermontane3@gmail.com>
2024-12-17 18:02:14 -0500

    fix: renamed ID to fix broken copy button #minor

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,5 +1,5 @@
 function copyText() {
-    let copyText = document.getElementById("password");
+    let copyText = document.getElementById("password-gen");
     copyText.select();
     navigator.clipboard.writeText(copyText.value);
 }
