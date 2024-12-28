76b47857fc0673f3b4b7f6dca2b66ae0764ecb68
xaviermontane <xaviermontane3@gmail.com>
2024-12-28 02:56:24 -0500

    feat(generate): set password length based on strength

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,5 +1,34 @@
+const strengthButton = document.getElementById("custom-strength");
+const passwordLengthInput = document.getElementById("length");
+
 function copyText() {
     let copyText = document.getElementById("password-gen");
     copyText.select();
     navigator.clipboard.writeText(copyText.value);
 }
+
+passwordLengthInput.addEventListener("input", (e) => {
+    passwordLengthInput.value = e.target.value;
+});
+
+strengthButton.addEventListener("change", (e) => {
+    const strength = e.target.value;
+    let newLength;
+    switch (strength) {
+        case "trolling":
+            newLength = 8;
+            break;
+        case "weak":
+            newLength = 8;
+            break;
+        case "secure":
+            newLength = 32;
+            break;
+        case "hax0r":
+            newLength = 64;
+            break;
+        default:
+            newLength = 12;
+    }
+    passwordLengthInput.value = newLength;
+});
